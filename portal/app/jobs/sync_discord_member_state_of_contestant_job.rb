6612377ca955d6c23ad8ed7af66135b8c8e55fd3
sapphi-red <green@sapphi.red>
2021-07-15 17:14:32 +0900

    サーバー参加イベント時にはDiscordのAPIを叩かないように

--- portal/app/jobs/sync_discord_member_state_of_contestant_job.rb
+++ portal/app/jobs/sync_discord_member_state_of_contestant_job.rb
@@ -1,16 +1,19 @@
 class SyncDiscordMemberStateOfContestantJob < ApplicationJob
   self.log_arguments = false
-  def perform(contestant)
+  def perform(contestant, force_is_member = nil)
     Rails.logger.info "Syncing discord guild member state of discord user #{contestant.discord_tag} (#{contestant.discord_id}), contestant_id=#{contestant.id}"
 
-    is_member = begin
-      Discordrb::API::Server.resolve_member(
-        discord_token,
-        server_id,
-        contestant.discord_id
-      )
-    rescue RestClient::NotFound => e
-      false
+    is_member = force_is_member
+    if is_member.nil?
+      is_member = begin
+        Discordrb::API::Server.resolve_member(
+          discord_token,
+          server_id,
+          contestant.discord_id
+        )
+      rescue RestClient::NotFound => e
+        false
+      end
     end
 
     contestant.update_attributes!(

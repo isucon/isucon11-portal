8dd616f0208a978d324ab7336caac23935fc500e
takonomura <takonomura@users.noreply.github.com>
2021-09-29 22:08:31 +0900

    Destroy SQS, ElastiCache resources

--- tf/cloudwatch-prd.tf
+++ tf/cloudwatch-prd.tf
@@ -1,41 +1,41 @@
-resource "aws_cloudwatch_metric_alarm" "ecs-scaling-out-worker-autoscaling-service-prd" {
-  alarm_name                = "ecs-scaling-out-worker-autoscaling-service-prd"
-  comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = "1"
-  metric_name               = "ApproximateNumberOfMessagesVisible"
-  namespace                 = "AWS/SQS"
-  dimensions                = {
-    QueueName = aws_sqs_queue.isuxportal-activejob-prd.name
-  }
-  period                    = "60"
-  statistic                 = "Average"
-  threshold                 = "20"
-  alarm_description         = "This metric monitors sqs ApproximateNumberOfMessagesVisible count"
-
-  lifecycle {
-    ignore_changes = [
-      alarm_actions, // Ignore changes by hako
-    ]
-  }
-}
-
-resource "aws_cloudwatch_metric_alarm" "ecs-scaling-in-worker-autoscaling-service-prd" {
-  alarm_name                = "ecs-scaling-in-worker-autoscaling-service-prd"
-  comparison_operator       = "LessThanThreshold"
-  evaluation_periods        = "1"
-  metric_name               = "ApproximateNumberOfMessagesVisible"
-  namespace                 = "AWS/SQS"
-  dimensions                = {
-    QueueName = aws_sqs_queue.isuxportal-activejob-prd.name
-  }
-  period                    = "60"
-  statistic                 = "Average"
-  threshold                 = "20"
-  alarm_description         = "This metric monitors sqs ApproximateNumberOfMessagesVisible count"
-
-  lifecycle {
-    ignore_changes = [
-      alarm_actions, // Ignore changes by hako
-    ]
-  }
-}
+#resource "aws_cloudwatch_metric_alarm" "ecs-scaling-out-worker-autoscaling-service-prd" {
+#  alarm_name                = "ecs-scaling-out-worker-autoscaling-service-prd"
+#  comparison_operator       = "GreaterThanOrEqualToThreshold"
+#  evaluation_periods        = "1"
+#  metric_name               = "ApproximateNumberOfMessagesVisible"
+#  namespace                 = "AWS/SQS"
+#  dimensions                = {
+#    QueueName = aws_sqs_queue.isuxportal-activejob-prd.name
+#  }
+#  period                    = "60"
+#  statistic                 = "Average"
+#  threshold                 = "20"
+#  alarm_description         = "This metric monitors sqs ApproximateNumberOfMessagesVisible count"
+#
+#  lifecycle {
+#    ignore_changes = [
+#      alarm_actions, // Ignore changes by hako
+#    ]
+#  }
+#}
+#
+#resource "aws_cloudwatch_metric_alarm" "ecs-scaling-in-worker-autoscaling-service-prd" {
+#  alarm_name                = "ecs-scaling-in-worker-autoscaling-service-prd"
+#  comparison_operator       = "LessThanThreshold"
+#  evaluation_periods        = "1"
+#  metric_name               = "ApproximateNumberOfMessagesVisible"
+#  namespace                 = "AWS/SQS"
+#  dimensions                = {
+#    QueueName = aws_sqs_queue.isuxportal-activejob-prd.name
+#  }
+#  period                    = "60"
+#  statistic                 = "Average"
+#  threshold                 = "20"
+#  alarm_description         = "This metric monitors sqs ApproximateNumberOfMessagesVisible count"
+#
+#  lifecycle {
+#    ignore_changes = [
+#      alarm_actions, // Ignore changes by hako
+#    ]
+#  }
+#}

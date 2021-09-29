f30065c975a1f25962986d429af54045895fa506
takonomura <takonomura@users.noreply.github.com>
2021-09-29 21:54:29 +0900

    Destroy all hako apps

--- tf/route53_common.tf
+++ tf/route53_common.tf
@@ -2,23 +2,23 @@
 # Production
 ########################################
 
-data "aws_lb" "hako-isuxportal-prd-fargate" {
-  name = "hako-isuxportal-prd-fargate"
-}
-
-resource "aws_route53_record" "cname_portal-prd-xi-isucon-dev" {
-  for_each = {
-    for k in [
-      aws_route53_zone.public.zone_id,
-      #aws_route53_zone.private.zone_id,
-    ] : k => k
-  }
-  zone_id = each.value
-  name    = "portal-prd.xi.isucon.dev"
-  type    = "CNAME"
-  ttl     = "300"
-  records = [data.aws_lb.hako-isuxportal-prd-fargate.dns_name]
-}
+#data "aws_lb" "hako-isuxportal-prd-fargate" {
+#  name = "hako-isuxportal-prd-fargate"
+#}
+#
+#resource "aws_route53_record" "cname_portal-prd-xi-isucon-dev" {
+#  for_each = {
+#    for k in [
+#      aws_route53_zone.public.zone_id,
+#      #aws_route53_zone.private.zone_id,
+#    ] : k => k
+#  }
+#  zone_id = each.value
+#  name    = "portal-prd.xi.isucon.dev"
+#  type    = "CNAME"
+#  ttl     = "300"
+#  records = [data.aws_lb.hako-isuxportal-prd-fargate.dns_name]
+#}
 
 #resource "aws_route53_record" "cname_portal-prd-isucon-net-xi-isucon-dev" {
 #  for_each = {
@@ -38,23 +38,23 @@ resource "aws_route53_record" "cname_portal-prd-xi-isucon-dev" {
 ########################################
 
 
-data "aws_lb" "hako-isuxportal-dev-fargate" {
-  name = "hako-isuxportal-dev-fargate"
-}
-
-resource "aws_route53_record" "cname_portal-dev-xi-isucon-dev" {
-  for_each = {
-    for k in [
-      aws_route53_zone.public.zone_id,
-      #aws_route53_zone.private.zone_id,
-    ] : k => k
-  }
-  zone_id = each.value
-  name    = "portal-dev.xi.isucon.dev"
-  type    = "CNAME"
-  ttl     = "300"
-  records = [data.aws_lb.hako-isuxportal-dev-fargate.dns_name]
-}
+#data "aws_lb" "hako-isuxportal-dev-fargate" {
+#  name = "hako-isuxportal-dev-fargate"
+#}
+#
+#resource "aws_route53_record" "cname_portal-dev-xi-isucon-dev" {
+#  for_each = {
+#    for k in [
+#      aws_route53_zone.public.zone_id,
+#      #aws_route53_zone.private.zone_id,
+#    ] : k => k
+#  }
+#  zone_id = each.value
+#  name    = "portal-dev.xi.isucon.dev"
+#  type    = "CNAME"
+#  ttl     = "300"
+#  records = [data.aws_lb.hako-isuxportal-dev-fargate.dns_name]
+#}
 
 #resource "aws_route53_record" "cname_portal-dev-isucon-net-xi-isucon-dev" {
 #  for_each = {
@@ -70,41 +70,41 @@ resource "aws_route53_record" "cname_portal-dev-xi-isucon-dev" {
 #  records = ["${aws_cloudfront_distribution.isuxportal-dev.domain_name}."]
 #}
 
-data "aws_lb" "hako-isuxportal-dev-grpc-fargate" {
-  name = "hako-isuxportal-dev-grpc-fargate"
-}
-
-resource "aws_route53_record" "cname_isuxportal-dev-grpc-xi-isucon-dev" {
-  for_each = {
-    for k in [
-      aws_route53_zone.public.zone_id,
-      #aws_route53_zone.private.zone_id,
-    ] : k => k
-  }
-  zone_id = each.value
-  name    = "isuxportal-dev-grpc.xi.isucon.dev"
-  type    = "CNAME"
-  ttl     = "300"
-  records = [data.aws_lb.hako-isuxportal-dev-grpc-fargate.dns_name]
-}
-
-data "aws_lb" "hako-isuxportal-prd-grpc-fargate" {
-  name = "hako-isuxportal-prd-grpc-fargate"
-}
-
-resource "aws_route53_record" "cname_isuxportal-prd-grpc-xi-isucon-dev" {
-  for_each = {
-    for k in [
-      aws_route53_zone.public.zone_id,
-      #aws_route53_zone.private.zone_id,
-    ] : k => k
-  }
-  zone_id = each.value
-  name    = "isuxportal-prd-grpc.xi.isucon.dev"
-  type    = "CNAME"
-  ttl     = "300"
-  records = [data.aws_lb.hako-isuxportal-prd-grpc-fargate.dns_name]
-}
+#data "aws_lb" "hako-isuxportal-dev-grpc-fargate" {
+#  name = "hako-isuxportal-dev-grpc-fargate"
+#}
+#
+#resource "aws_route53_record" "cname_isuxportal-dev-grpc-xi-isucon-dev" {
+#  for_each = {
+#    for k in [
+#      aws_route53_zone.public.zone_id,
+#      #aws_route53_zone.private.zone_id,
+#    ] : k => k
+#  }
+#  zone_id = each.value
+#  name    = "isuxportal-dev-grpc.xi.isucon.dev"
+#  type    = "CNAME"
+#  ttl     = "300"
+#  records = [data.aws_lb.hako-isuxportal-dev-grpc-fargate.dns_name]
+#}
+#
+#data "aws_lb" "hako-isuxportal-prd-grpc-fargate" {
+#  name = "hako-isuxportal-prd-grpc-fargate"
+#}
+#
+#resource "aws_route53_record" "cname_isuxportal-prd-grpc-xi-isucon-dev" {
+#  for_each = {
+#    for k in [
+#      aws_route53_zone.public.zone_id,
+#      #aws_route53_zone.private.zone_id,
+#    ] : k => k
+#  }
+#  zone_id = each.value
+#  name    = "isuxportal-prd-grpc.xi.isucon.dev"
+#  type    = "CNAME"
+#  ttl     = "300"
+#  records = [data.aws_lb.hako-isuxportal-prd-grpc-fargate.dns_name]
+#}
 
 #data "aws_lb" "hako-isuxportal-pre-grpc-fargate" {
 #  name = "hako-isuxportal-pre-grpc-fargate"
